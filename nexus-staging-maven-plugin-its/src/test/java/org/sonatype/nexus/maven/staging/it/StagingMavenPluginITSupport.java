67afaa60044fb64f7c29750f318f1ea964514106
Tamas Cservenak <tamas@cservenak.net>
2012-07-24 11:50:21 +0200

    Improved logging and reuse of already expanded Mavens

--- nexus-staging-maven-plugin-its/src/test/java/org/sonatype/nexus/maven/staging/it/StagingMavenPluginITSupport.java
+++ nexus-staging-maven-plugin-its/src/test/java/org/sonatype/nexus/maven/staging/it/StagingMavenPluginITSupport.java
@@ -1,3 +1,15 @@
+/**
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2012 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
 package org.sonatype.nexus.maven.staging.it;
 
 import static org.sonatype.nexus.client.rest.BaseUrl.baseUrlFrom;
@@ -107,17 +119,25 @@ public abstract class StagingMavenPluginITSupport
     public void resolveMavens()
         throws IOException
     {
-        logger.info( "Setting up Mavens..." );
+        logger.info( "Setting up Maven binaries..." );
         final File mavenHomesBase = new File( getBasedir(), "target/maven" );
         for ( String mavenVersion : getMavenVersions() )
         {
             final File mavenHome = new File( mavenHomesBase, MAVEN_A + "-" + mavenVersion );
-            logger.info( "  Setting up Maven " + mavenVersion + "..." );
-            final File maven2bundle = resolveArtifact( MAVEN_G + ":" + MAVEN_A + ":zip:bin:" + mavenVersion );
-            fileTaskBuilder.expand( file( maven2bundle ) ).to().directory( file( mavenHomesBase ) ).run();
-            fileTaskBuilder.chmod( file( new File( mavenHome, "bin" ) ) ).include( "mvn" ).permissions( "755" ).run();
+            if ( !mavenHome.isDirectory() )
+            {
+                logger.info( "  Expanding Maven " + mavenVersion + "..." );
+                final File maven2bundle = resolveArtifact( MAVEN_G + ":" + MAVEN_A + ":zip:bin:" + mavenVersion );
+                fileTaskBuilder.expand( file( maven2bundle ) ).to().directory( file( mavenHomesBase ) ).run();
+                fileTaskBuilder.chmod( file( new File( mavenHome, "bin" ) ) ).include( "mvn" ).permissions( "755" ).run();
+            }
+            else
+            {
+                logger.info( "  Reusing Maven " + mavenVersion + "..." );
+            }
             mavenHomes.put( mavenVersion, mavenHome );
         }
+        logger.info( "  Maven binaries set up..." );
     }
 
     @Before
