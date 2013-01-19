a16bebf70a8ed0ea863720969283e7a7f2fb839e
Jason Dillon <jason@planet57.com>
2013-01-18 17:56:10 -0800

    Some tidy (shit there is a lot more) And start of m2settings plugin support.

---
+++ m2settings/maven-plugin/src/main/java/org/sonatype/nexus/maven/m2settings/DownloadMojo.java
@@ -0,0 +1,21 @@
+package org.sonatype.nexus.maven.m2settings;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * ???
+ *
+ * @since 1.4
+ */
+@Mojo(name="download")
+public class DownloadMojo
+    extends AbstractMojo
+{
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        // TODO
+    }
+}
