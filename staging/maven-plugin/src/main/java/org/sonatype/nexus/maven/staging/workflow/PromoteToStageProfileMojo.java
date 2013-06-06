1811cd35a4e30b0f5f74d2a741645d6b51ce1377
Tamas Cservenak <tamas@cservenak.net>
2013-06-06 14:48:24 +0200

    NXCM-5297: Improved console message.
    Also, applied to RC goal too. Special
    message explains why ID is not available.

--- staging/maven-plugin/src/main/java/org/sonatype/nexus/maven/staging/workflow/PromoteToStageProfileMojo.java
+++ staging/maven-plugin/src/main/java/org/sonatype/nexus/maven/staging/workflow/PromoteToStageProfileMojo.java
@@ -20,6 +20,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.sonatype.nexus.maven.staging.StagingAction;
 
+import com.google.common.base.Strings;
 import com.sonatype.nexus.staging.client.StagingWorkflowV2Service;
 
 /**
@@ -62,6 +63,13 @@ public class PromoteToStageProfileMojo
         final String promotionGroupId =
             stagingWorkflow.promoteStagingRepositories( getDescriptionWithDefaultsForAction( StagingAction.PROMOTE ),
                 getBuildPromotionProfileId(), getStagingRepositoryIds() );
-        getLog().info( "Promoted, created promotion group with ID " + promotionGroupId );
+        if ( Strings.isNullOrEmpty( promotionGroupId ) )
+        {
+            getLog().info( "Promoted, but created promotion group ID unknown (needs Nexus Pro version 2.4+)." );
+        }
+        else
+        {
+            getLog().info( "Promoted, created promotion group with ID " + promotionGroupId );
+        }
     }
 }
