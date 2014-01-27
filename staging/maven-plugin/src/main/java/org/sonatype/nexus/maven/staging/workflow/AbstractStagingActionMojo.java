7a3fb8217fcfd0cf5ecc935787a06e7f37f77dc5
Tamas Cservenak <tamas@cservenak.net>
2014-01-27 14:46:25 +0100

    NEXUs-6182: Get rid of dupe code

--- staging/maven-plugin/src/main/java/org/sonatype/nexus/maven/staging/workflow/AbstractStagingActionMojo.java
+++ staging/maven-plugin/src/main/java/org/sonatype/nexus/maven/staging/workflow/AbstractStagingActionMojo.java
@@ -13,37 +13,19 @@
 
 package org.sonatype.nexus.maven.staging.workflow;
 
-import java.net.MalformedURLException;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.sonatype.nexus.staging.client.StagingRuleFailuresException;
 import com.sonatype.nexus.staging.client.StagingWorkflowV2Service;
-import com.sonatype.nexus.staging.client.StagingWorkflowV3Service;
-import com.sonatype.nexus.staging.client.rest.JerseyStagingWorkflowV2SubsystemFactory;
-import com.sonatype.nexus.staging.client.rest.JerseyStagingWorkflowV3SubsystemFactory;
 
-import org.sonatype.maven.mojo.settings.MavenSettings;
 import org.sonatype.nexus.client.core.NexusClient;
 import org.sonatype.nexus.client.core.exception.NexusClientErrorResponseException;
-import org.sonatype.nexus.client.rest.BaseUrl;
-import org.sonatype.nexus.client.rest.ConnectionInfo;
-import org.sonatype.nexus.client.rest.ConnectionInfo.ValidationLevel;
-import org.sonatype.nexus.client.rest.Protocol;
-import org.sonatype.nexus.client.rest.ProxyInfo;
-import org.sonatype.nexus.client.rest.UsernamePasswordAuthenticationInfo;
-import org.sonatype.nexus.client.rest.jersey.JerseyNexusClientFactory;
 import org.sonatype.nexus.maven.staging.AbstractStagingMojo;
 import org.sonatype.nexus.maven.staging.ErrorDumper;
-import org.sonatype.nexus.maven.staging.ProgressMonitorImpl;
 import org.sonatype.nexus.maven.staging.StagingAction;
-import org.sonatype.plexus.components.sec.dispatcher.SecDispatcherException;
+import org.sonatype.nexus.maven.staging.remote.Parameters;
+import org.sonatype.nexus.maven.staging.remote.RemoteNexus;
 
-import com.sun.jersey.api.client.UniformInterfaceException;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.settings.Proxy;
-import org.apache.maven.settings.Server;
 
 /**
  * Super class of non-RC Actions. These mojos are "plain" non-aggregator ones, and will use the property file from
@@ -69,8 +51,7 @@ public abstract class AbstractStagingActionMojo
 
     if (shouldExecute()) {
       getLog().info("Connecting to Nexus...");
-      createTransport(getNexusUrl());
-      final StagingWorkflowV2Service stagingWorkflow = getStagingWorkflowService();
+      final StagingWorkflowV2Service stagingWorkflow = createStagingWorkflowService();
 
       try {
         doExecute(stagingWorkflow);
@@ -106,144 +87,21 @@ public abstract class AbstractStagingActionMojo
   /**
    * Initialized stuff needed for transport, stuff like: Server, Proxy and RemoteNexus.
    */
-  protected void createTransport(final String deployUrl)
+  protected StagingWorkflowV2Service createStagingWorkflowService()
       throws MojoExecutionException
   {
-    if (deployUrl == null) {
-      throw new MojoExecutionException(
-          "The URL against which transport should be established is not defined! (use \"-DnexusUrl=http://host/nexus\" on CLI or configure it in POM)");
-    }
-
-    final Server server;
-    final Proxy proxy;
-
     try {
-      if (getServerId() != null) {
-        final Server selectedServer =
-            MavenSettings.selectServer(getMavenSession().getSettings(), getServerId());
-        if (selectedServer != null) {
-          server = MavenSettings.decrypt(getSecDispatcher(), selectedServer);
-        }
-        else {
-          throw new MojoExecutionException("Server credentials with ID \"" + getServerId() + "\" not found!");
-        }
-      }
-      else {
-        throw new MojoExecutionException(
-            "Server credentials to use in transport are not defined! (use \"-DserverId=someServerId\" on CLI or configure it in POM)");
-      }
-
-      final Proxy selectedProxy = MavenSettings.selectProxy(getMavenSession().getSettings(), deployUrl);
-      if (selectedProxy != null) {
-        proxy = MavenSettings.decrypt(getSecDispatcher(), selectedProxy);
-      }
-      else {
-        proxy = null;
-      }
-
-      try {
-        final BaseUrl baseUrl = BaseUrl.baseUrlFrom(getNexusUrl());
-        final UsernamePasswordAuthenticationInfo authenticationInfo;
-        final Map<Protocol, ProxyInfo> proxyInfos = new HashMap<Protocol, ProxyInfo>(1);
-
-        if (server != null && server.getUsername() != null) {
-          getLog().info("Using server credentials with ID=\"" + server.getId() + "\" from Maven settings.");
-          authenticationInfo =
-              new UsernamePasswordAuthenticationInfo(server.getUsername(), server.getPassword());
-        }
-        else {
-          authenticationInfo = null;
-        }
-
-        if (proxy != null) {
-          getLog().info(
-              "Using " + proxy.getProtocol().toUpperCase() + " Proxy with ID=\"" + proxy.getId()
-                  + "\" from Maven settings.");
-          final UsernamePasswordAuthenticationInfo proxyAuthentication;
-          if (proxy.getUsername() != null) {
-            proxyAuthentication =
-                new UsernamePasswordAuthenticationInfo(proxy.getUsername(), proxy.getPassword());
-          }
-          else {
-            proxyAuthentication = null;
-          }
-          final ProxyInfo zProxy =
-              new ProxyInfo(Protocol.valueOf(proxy.getProtocol().toUpperCase()), proxy.getHost(),
-                  proxy.getPort(), proxyAuthentication);
-          proxyInfos.put(zProxy.getProxyProtocol(), zProxy);
-        }
-
-        final ValidationLevel sslCertificateValidationLevel = isSslInsecure() ? ValidationLevel.LAX : ValidationLevel.STRICT;
-        final ValidationLevel sslCertificateHostnameValidationLevel = isSslAllowAll() ? ValidationLevel.NONE : ValidationLevel.LAX;
-        final ConnectionInfo connectionInfo = new ConnectionInfo(baseUrl, authenticationInfo, proxyInfos,
-            sslCertificateValidationLevel, sslCertificateHostnameValidationLevel);
-
-        this.nexusClient =
-            new JerseyNexusClientFactory(
-                // support v2 and v3
-                new JerseyStagingWorkflowV2SubsystemFactory(), new JerseyStagingWorkflowV3SubsystemFactory())
-                .createFor(connectionInfo);
-
-        getLog().debug("RemoteNexus created against Nexus instance on URL: " + baseUrl.toString() + ".");
-      }
-      catch (MalformedURLException e) {
-        throw new MojoExecutionException("Malformed Nexus base URL!", e);
-      }
-      catch (UniformInterfaceException e) {
-        throw new MojoExecutionException("Nexus base URL does not point to a valid Nexus location: "
-            + e.getMessage(), e);
-      }
-      catch (Exception e) {
-        throw new MojoExecutionException("Nexus connection problem: " + e.getMessage(), e);
-      }
+      final Parameters parameters = new Parameters(getPluginGav(), getDeferredDirectoryRoot(),
+          getStagingDirectoryRoot());
+      parameters.setNexusUrl(getNexusUrl());
+      parameters.setServerId(getServerId());
+      parameters.validateRemoting();
+      final RemoteNexus remoteNexus = new RemoteNexus(getMavenSession(), getSecDispatcher(), getLog().isDebugEnabled(),
+          parameters);
+      return remoteNexus.getStagingWorkflowV2Service();
     }
-    catch (SecDispatcherException e) {
-      throw new MojoExecutionException("Cannot decipher credentials to be used with Nexus!", e);
+    catch (Exception e) {
+      throw new MojoExecutionException("Nexus connection problem: " + e.getMessage(), e);
     }
-    catch (MalformedURLException e) {
-      throw new MojoExecutionException("Malformed Nexus base URL!", e);
-    }
-  }
-
-  // FIXME: This is duplicated in RemotingImpl
-
-  private StagingWorkflowV2Service workflowService;
-
-  protected StagingWorkflowV2Service getStagingWorkflowService()
-      throws MojoExecutionException
-  {
-    if (workflowService == null) {
-      // First try v3
-      try {
-        StagingWorkflowV3Service service = nexusClient.getSubsystem(StagingWorkflowV3Service.class);
-
-        getLog().debug("Using staging v3 service");
-
-        // configure progress monitor
-        service.setProgressMonitor(new ProgressMonitorImpl(getLog()));
-        service.setProgressTimeoutMinutes(getStagingProgressTimeoutMinutes());
-        service.setProgressPauseDurationSeconds(getStagingProgressPauseDurationSeconds());
-
-        workflowService = service;
-      }
-      catch (Exception e) {
-        getLog().debug("Unable to resolve staging v3 service; falling back to v2", e);
-      }
-
-      if (workflowService == null) {
-        // fallback to v2 if v3 not available
-        try {
-          workflowService = nexusClient.getSubsystem(StagingWorkflowV2Service.class);
-          getLog().debug("Using staging v2 service");
-        }
-        catch (IllegalArgumentException e) {
-          throw new MojoExecutionException(String.format(
-              "Nexus instance at base URL %s does not support staging v2; reported status: %s, reason:%s",
-              nexusClient.getConnectionInfo().getBaseUrl(), nexusClient.getNexusStatus(), e.getMessage()), e);
-        }
-      }
-    }
-
-    return workflowService;
   }
 }
