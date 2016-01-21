ac260eb5fa93a3e29309fe2a3b0f12929c9c7767
David Ashirov <david.ashirov@mindspark.com>
2016-01-21 00:24:53 -0500

    Added "rc-open" remote control mojo to start a new staging repository from a staging profile    uses same stagingDescription parameter as convention was already long established    uses new stagingProfileId parameter    does not require stagingRepositoryId parameter (and thus extends a different parent)    has not yet been tested (how do you test mojos?)

---
+++ staging/maven-plugin/src/main/java/org/sonatype/nexus/maven/staging/workflow/rc/RcOpenStageRepositoryMojo.java
@@ -0,0 +1,51 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2015 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+
+package org.sonatype.nexus.maven.staging.workflow.rc;
+
+import com.sonatype.nexus.staging.client.*;
+import org.apache.maven.plugins.annotations.Mojo;
+import java.util.*;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+
+import org.sonatype.nexus.maven.staging.workflow.AbstractStagingActionMojo;
+
+/**
+ * Opens a new Nexus staging repository.
+ *
+ * @author dashirov
+ * @since 1.6.7
+ */
+@Mojo(name = "rc-open", requiresProject = false, requiresDirectInvocation = true, requiresOnline = true)
+public class RcOpenStageRepositoryMojo
+        extends AbstractStagingActionMojo
+{
+    @Parameter(property = "stagingProfileId", required = true)
+    private String stagingProfileId;
+
+    @Parameter(property = "stagingDescription", required = true)
+    private String stagingDescription;
+
+    @Override
+    public void doExecute(final StagingWorkflowV2Service stagingWorkflow)
+            throws MojoExecutionException, MojoFailureException
+    {
+        getLog().info("RC-Starting new staging repository with Profile ID: " + stagingProfileId);
+        final String stagingRepositoryId = stagingWorkflow.startStaging(stagingWorkflow.selectProfile(stagingProfileId), stagingDescription, null );
+        getLog().info("Opened new staging repository with Id: " + stagingRepositoryId);
+    }
+}
